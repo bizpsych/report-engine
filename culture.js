6f122a706e98693489f22e556be510a3d395d12b
bizpsych <69421922+bizpsych@users.noreply.github.com>
2020-08-25 22:41:05 +0300

    grit & culture

---
+++ culture.js
@@ -0,0 +1,20 @@
+$("#assertiveness").css( "width", assertiveness * 10 + '%');
+$("#assertiveness").css( "height",assertiveness * 10 + '%');
+
+$("#security").css( "width", security * 10 + '%');
+$("#security").css( "height",security * 10 + '%');
+
+$("#thinking").css( "width", thinking * 10 + '%');
+$("#thinking").css( "height",thinking * 10 + '%');
+
+$("#confidence").css( "width", confidence * 10 + '%');
+$("#confidence").css( "height",confidence * 10 + '%');
+
+$("#trust").css( "width", trust * 10 + '%');
+$("#trust").css( "height",trust * 10 + '%');
+
+$("#composure").css( "width", composure * 10 + '%');
+$("#composure").css( "height",composure * 10 + '%');
+
+$("#ideas").css( "width", ideas * 10 + '%');
+$("#ideas").css( "height",ideas * 10 + '%');
---
+++ grit.js
@@ -0,0 +1,20 @@
+  $("#diligence").css( "width", diligence * 10 + '%');
+  $("#diligence").css( "height",diligence * 10 + '%');
+
+  $("#leadership").css( "width", leadership * 10 + '%');
+  $("#leadership").css( "height",leadership * 10 + '%');
+
+  $("#seeking").css( "width", seeking * 10 + '%');
+  $("#seeking").css( "height",seeking * 10 + '%');
+
+  $("#intelect").css( "width", intelect * 10 + '%');
+  $("#intelect").css( "height",intelect * 10 + '%');
+
+  $("#success").css( "width", success * 10 + '%');
+  $("#success").css( "height",success * 10 + '%');
+
+  $("#autonomy").css( "width", autonomy * 10 + '%');
+  $("#autonomy").css( "height",autonomy * 10 + '%');
+
+  $("#persistence").css( "width", persistence * 10 + '%');
+  $("#persistence").css( "height",persistence * 10 + '%');
