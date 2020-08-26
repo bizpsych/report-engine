bcc64b011e1554edeb36a07f9c3683419cbf49e6
bizpsych <69421922+bizpsych@users.noreply.github.com>
2020-08-26 12:58:09 +0300

    Update resilience.js

--- resilience.js
+++ resilience.js
@@ -22,74 +22,39 @@ $(document).ready(function() {
   $("#adversity").css( "height",adversity * 10 + '%');
 
 //Resilience Conditions
-  if (optimism < 7.6) {
+  if (7.6 < optimism && optimism < 4.6) {
     $("#optimism").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#optimism-label").css("background", "#C18625");
     $("#optimism-score").css("color", "#ECA327");
 	}
-  if (optimism < 4.6) {
-  	$("#optimism").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#optimism-label").css("background", "#CE4427");
-    $("#optimism-score").css("color", "#F25C24");
-	}
-  if (efficacy < 7.6) {
+  if (7.6 < efficacy && efficacy < 4.6) {
     $("#efficacy").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#efficacy").css("background", "#C18625");
     $("#efficacy-score").css("color", "#ECA327");
 	}
-  if (efficacy < 4.6) {
-  	$("#efficacy").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#efficacy-label").css("background", "#CE4427");
-    $("#efficacy-score").css("color", "#F25C24");
-	}
-  if (determination < 7.6) {
+  if (7.6 < determination && determination < 4.6) {
     $("#determination").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#determination-label").css("background", "#C18625");
     $("#determination-score").css("color", "#ECA327");
   }
-  if (determination < 4.6) {
-    $("#determination").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#determination-label").css("background", "#CE4427");
-    $("#determination-score").css("color", "#F25C24");
-  }
-  if (temper < 7.6) {
+  if (7.6 < temper && temper < 4.6) {
     $("#temper").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#temper-label").css("background", "#C18625");
     $("#temper-score").css("color", "#ECA327");
   }
-  if (temper < 4.6) {
-    $("#temper").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#temper-label").css("background", "#CE4427");
-    $("#temper-score").css("color", "#F25C24");
-  }
-  if (assured < 7.6) {
+  if (7.6 < assured && assured < 4.6) {
     $("#assured").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#assured-label").css("background", "#C18625");
     $("#assured-score").css("color", "#ECA327");
   }
-  if (assured < 4.6) {
-    $("#assured").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#assured-label").css("background", "#CE4427");
-    $("#assured-score").css("color", "#F25C24");
-  }
-  if (positive < 7.6) {
+  if (7.6 < positive && positive < 4.6) {
     $("#positive").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#positive-label").css("background", "#C18625");
     $("#positive-score").css("color", "#ECA327");
   }
-  if (positive < 4.6) {
-    $("#positive").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#positive-label").css("background", "#CE4427");
-    $("#positive-score").css("color", "#F25C24");
-  }
-  if (adversity < 7.6) {
+  if (7.6 < adversity && adversity < 4.6) {
     $("#adversity").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#adversity-label").css("background", "#C18625");
     $("#adversity-score").css("color", "#ECA327");
-  }
-  if (adversity < 4.6) {
-    $("#adversity").css("border", "4px solid #CE4427").css("background", "rgba(206, 68, 39, 0.2)");
-    $("#adversity-label").css("background", "#CE4427");
-    $("#adversity-score").css("color", "#F25C24");
   }
 });
