edb94dd0e0b561cdacba3586f86c4ad8e97e3b29
bizpsych <69421922+bizpsych@users.noreply.github.com>
2020-08-26 13:09:43 +0300

    Update resilience.js

--- resilience.js
+++ resilience.js
@@ -22,39 +22,9 @@ $(document).ready(function() {
   $("#adversity").css( "height",adversity * 10 + '%');
 
 //Resilience Conditions
-  if (7.6 < optimism && optimism < 4.6) {
+  if (optimism >= 4.5 && optimism <= 7.5 {
     $("#optimism").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
     $("#optimism-label").css("background", "#C18625");
     $("#optimism-score").css("color", "#ECA327");
 	}
-  if (7.6 < efficacy && efficacy < 4.6) {
-    $("#efficacy").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#efficacy").css("background", "#C18625");
-    $("#efficacy-score").css("color", "#ECA327");
-	}
-  if (7.6 < determination && determination < 4.6) {
-    $("#determination").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#determination-label").css("background", "#C18625");
-    $("#determination-score").css("color", "#ECA327");
-  }
-  if (7.6 < temper && temper < 4.6) {
-    $("#temper").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#temper-label").css("background", "#C18625");
-    $("#temper-score").css("color", "#ECA327");
-  }
-  if (7.6 < assured && assured < 4.6) {
-    $("#assured").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#assured-label").css("background", "#C18625");
-    $("#assured-score").css("color", "#ECA327");
-  }
-  if (7.6 < positive && positive < 4.6) {
-    $("#positive").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#positive-label").css("background", "#C18625");
-    $("#positive-score").css("color", "#ECA327");
-  }
-  if (7.6 < adversity && adversity < 4.6) {
-    $("#adversity").css("border", "4px solid #C18625").css("background", "rgba(193, 134, 37, 0.2)");
-    $("#adversity-label").css("background", "#C18625");
-    $("#adversity-score").css("color", "#ECA327");
-  }
 });
